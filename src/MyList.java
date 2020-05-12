4b1d47fe35c8b960b64a9473a6722b850362b06b
Kira <datdhvinh@gmail.com>
2020-05-12 16:35:35 +0700

    sua loi

--- src/MyList.java
+++ src/MyList.java
@@ -1,35 +1,35 @@
 import java.util.Arrays;
 
 public class MyList<E> {
-    private int size=0;
-    private static final int DEFAULT_CAPACITY=10;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
 
-    protected MyList(){
-        elements =new Object[DEFAULT_CAPACITY];
+    protected MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
     //Tăng gấp đôi kích thước mảng chứa các phần tử
-    public void ensureCapacity(){
-        int newSize=elements.length*2;
-        elements= Arrays.copyOf(elements,newSize);
+    public void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
     }
 
     //thêm một phần tử vào cuối của danh sách
     //Tham số đầu vào: phần tử cần thêm vào danh sách
-    public void add(E e){
-        if (size==elements.length){
+    public void add(E e) {
+        if (size == elements.length) {
             ensureCapacity();
         }
-        elements[size++]=e;
+        elements[size++] = e;
     }
 
-    public E get(int i){
-        if (i>=size||i<0){
-            throw new IndexOutOfBoundsException("Err!, Index: "+i+", Size: "+size);
+    //Trả về phần tử vừa ở vị trí thứ i trong danh sách.
+    //Tham số đầu vào: số nguyên chứa vị trí cần truy cập đến phần tử trong danh sách
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Err!, Index: " + index + ", Size: " + size);
         }
-        return (E) elements[i];
+        return (E) elements[index];
     }
-
-
 }
